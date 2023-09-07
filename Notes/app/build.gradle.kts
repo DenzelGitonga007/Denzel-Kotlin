989badfdf9fee9c0607ea61a5dec89787cb55fa1
Denzel Gitonga <gitongadenzel@gmail.com>
2023-09-07 11:38:38 +0300

    project setup

--- Notes/app/build.gradle.kts
+++ Notes/app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+//    Additional plugins
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -38,6 +42,7 @@ android {
     }
     buildFeatures {
         compose = true
+        dataBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -47,16 +52,19 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
+
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
+    //noinspection GradleCompatible
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
@@ -66,4 +74,56 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+// Additional dependencies
+    //noinspection GradleCompatible
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    val lifecycle_version = "2.6.1"
+    val room_version = "2.6.0-beta01"
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+
+    // RecyclerView Animator
+    implementation("jp.wasabeef:recyclerview-animators:3.0.0")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
+
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // Annotation processor
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
+
+    //Room
+    implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+
+    //Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+
+    // Navigation Components
+    implementation("androidx.fragment:fragment-ktx:1.7.0-alpha04")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+
+    //material Components
+    implementation("com.google.android.material:material:1.11.0-alpha02")
+
+    //color picker library
+    implementation("com.thebluealliance:spectrum:0.7.1")
+
+    implementation("com.yahiaangelo.markdownedittext:markdownedittext:1.1.1")
+    implementation("io.noties.markwon:core:4.6.0")
+    implementation("io.noties.markwon:ext-strikethrough:4.6.0")
+    implementation("io.noties.markwon:ext-tasklist:4.6.0")
+
 }
